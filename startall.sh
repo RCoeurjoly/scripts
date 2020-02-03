050f9941f65d563bf4c65e04e6805ee65235c3f1
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2020-02-03 09:12:25 +0100

    Piezas

---
+++ startall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo /home/drcoeurjoly/DF5555xx/script/DF5555xx start
+/home/drcoeurjoly/MD5555xx/script/MD5555xx start
+/home/drcoeurjoly/SS5555xx/script/SS5555xx start
+sudo /data/programs/oms/RT5555xx/script/RT5555xx start
